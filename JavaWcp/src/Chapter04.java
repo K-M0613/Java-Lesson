62fb3f5c4951f8915e96c864d29167cca4d9564f
81805 <81805@DESKTOP-5I4FCDQ.elecom>
2022-09-24 16:16:17 +0900

    first commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+    public static void main(String[] args) {
+    	String name;
+    	name = "門馬一樹";
+    	System.out.println(name);
+    	
+    	name = "好きな言葉";
+    	System.out.println(name);
+    	
+    	final int rucknum = 7;
+    	System.out.println(rucknum);
+    }
+}
---
+++ JavaWcp/src/Chapter5.java
@@ -0,0 +1,27 @@
+
+public class Chapter5 {
+    public static void main(String[] args) {
+    	long long1;
+    	long1 = 10000000L;
+    	System.out.println(long1);
+    	
+    	double double1;
+    	double1 = 1234567.89;
+    	System.out.println(double1);
+    	
+    	boolean boolean1 = true;
+    	System.out.println(boolean1);
+    	
+    	Integer i = 100;
+    	System.out.println(i);
+    	
+    	Float f = 1.14142f;
+    	System.out.println(f);
+    	
+    	Character c = 'う';
+    	System.out.println(c);
+    	
+    	int[] array = { 0, 10, 20, 30, 40 };
+    	System.out.println(array[3]);
+    }
+}
